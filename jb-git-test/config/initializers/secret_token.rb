0652b3ac74cc234e3f8db20fafac4c9845fadc34
jblair3 <theblairs090206@gmail.com>
2014-01-29 20:52:03 -0500

    initial commit

---
+++ jb-git-test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JbGitTest::Application.config.secret_key_base = 'd7422e5952414e495ae9b5de84b976f954a6e1e8d26738f2c993c54bf97365cc5c3b73c73e63b5c296c082841abde53ddeff500f6d7f6279e793a43dfbfd4566'
---
+++ jb-git-test/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JbGitTest::Application.config.session_store :cookie_store, key: '_jb-git-test_session'
